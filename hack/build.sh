f338d9098bee303748637279fa18c80331f5e923
Heathcliff <heathcliff@heathcliff.eu>
2024-01-03 19:44:44 +0100

    Add version information to binaries built with release workflow
    Fix debug.BuildInfo not containing any git tags.
    
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- hack/build.sh
+++ hack/build.sh
@@ -9,7 +9,11 @@ bin_dir="${base_dir}/bin"
 GOOS="${GOOS:-$(go env GOOS)}"
 GOARCH="${GOARCH:-$(go env GOARCH)}"
 
-GO_BUILD_FLAGS="${GO_BUILD_FLAGS:--ldflags="-w -s"}"
+GO_LD_FLAGS="${GO_LD_FLAGS:-"-w -s"} -X github.com/heathcliff26/infraspace-savegame-editor/pkg/cli.GitCommit=$(git rev-parse HEAD)"
+
+if [ ! -z "${RELEASE_VERSION}" ]; then
+    GO_LD_FLAGS="${GO_LD_FLAGS} -X github.com/heathcliff26/infraspace-savegame-editor/pkg/cli.Version=${RELEASE_VERSION}"
+fi
 
 output_name="${bin_dir}/infraspace-savegame-editor_${GOOS}_${GOARCH}"
 
@@ -20,4 +24,4 @@ fi
 pushd "${base_dir}" >/dev/null
 
 echo "Building $(basename "${output_name}")"
-GOOS="${GOOS}" GOARCH="${GOARCH}" go build "${GO_BUILD_FLAGS}" -o "${output_name}" ./cmd/save-editor/...
+GOOS="${GOOS}" GOARCH="${GOARCH}" go build -ldflags="${GO_LD_FLAGS}" -o "${output_name}" ./cmd/save-editor/...
