1d20149a37239098b307549ef4660e8521268e88
Heathcliff <heathcliff@heathcliff.eu>
2024-01-24 12:00:19 +0100

    CI: Fix go coverprofile report workflow
    Speed up the workflow and fix missing dependencies by using the container
    image instead of running directly on the runner.
    
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- hack/coverprofile.sh
+++ hack/coverprofile.sh
@@ -6,13 +6,12 @@ base_dir="$(dirname "${BASH_SOURCE[0]}" | xargs realpath)/.."
 
 pushd "${base_dir}" >/dev/null
 
-OUT_DIR="${base_dir}/coverprofiles"
+OUT_DIR="coverprofiles"
 
 if [ ! -d "${OUT_DIR}" ]; then
     mkdir "${OUT_DIR}"
 fi
 
-go test -coverprofile="${OUT_DIR}/cover.out" "./..."
-go tool cover -html "${OUT_DIR}/cover.out" -o "${OUT_DIR}/index.html"
+podman run -t -v "${base_dir}":/app:z ghcr.io/heathcliff26/go-fyne-ci:latest /bin/bash -c "go test -coverprofile=\"${OUT_DIR}/cover.out\" ./... && go tool cover -html \"${OUT_DIR}/cover.out\" -o \"${OUT_DIR}/index.html\""
 
 popd >/dev/null
