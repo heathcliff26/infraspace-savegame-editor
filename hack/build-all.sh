25f4c9e3870b83ae0f76d32ee34d538aca7de49a
Heathcliff <heathcliff@heathcliff.eu>
2024-02-09 15:48:13 +0100

    fyne-cross: Use custom fyne-cross image
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- hack/build-all.sh
+++ hack/build-all.sh
@@ -6,12 +6,6 @@ base_dir="$(dirname "${BASH_SOURCE[0]}" | xargs realpath)/.."
 
 declare -a ALL_OS=("windows" "linux")
 
-fyne_cross="$(go env GOPATH)/bin/fyne-cross"
-
-if [ ! -f "${fyne_cross}" ]; then
-    go install github.com/fyne-io/fyne-cross@latest
-fi
-
 for os in "${ALL_OS[@]}"; do
     "${base_dir}/hack/fyne-cross.sh" "${os}" amd64,arm64
 done
