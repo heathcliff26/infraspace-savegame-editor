13ac115936981799a36e81ff7a9e71f5d2921b24
Heathcliff <heathcliff@heathcliff.eu>
2025-01-01 10:47:05 +0100

    Improve install experience on linux
    Improve the installation experience on linux by adding a desktop file and
    bundling it into the release artifact.
    Add a script for installing the desktop file on the target system.
    
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- hack/fyne-cross.sh
+++ hack/fyne-cross.sh
@@ -4,9 +4,10 @@ set -e
 
 base_dir="$(dirname "${BASH_SOURCE[0]}" | xargs realpath)/.."
 
+FYNE_CROSS_IMAGE="ghcr.io/heathcliff26/go-fyne-ci:latest"
+
 os="${1}"
 arches="${2:-$(go env GOARCH)}"
-fyne_cross_image="ghcr.io/heathcliff26/go-fyne-ci:latest"
 
 fyne_cross="$(go env GOPATH)/bin/fyne-cross"
 
@@ -16,7 +17,7 @@ fi
 
 pushd "${base_dir}" >/dev/null
 
-${fyne_cross} "${os}" -arch="${arches}" -image="${fyne_cross_image}" ./cmd/save-editor/
+${fyne_cross} "${os}" -arch="${arches}" -image="${FYNE_CROSS_IMAGE}" ./cmd/save-editor/
 
 IFS=',' read -ra arch_array <<<"${arches}"
 
@@ -24,7 +25,14 @@ for arch in "${arch_array[@]}"; do
     if [ "${os}" == "linux" ]; then
         mv "fyne-cross/bin/linux-${arch}/save-editor" "fyne-cross/bin/linux-${arch}/infraspace-savegame-editor"
         rm -rf "fyne-cross/dist/linux-${arch}"
-        tar -C "fyne-cross/bin/linux-${arch}" -czf "fyne-cross/dist/infraspace-savegame-editor_linux-${arch}.tar.gz" infraspace-savegame-editor
+
+        echo "Building actual package fyne-cross/dist/infraspace-savegame-editor_linux-${arch}.tar.gz"
+        tmp_dir="fyne-cross/tmp/linux-${arch}-packaging"
+        [ -e "${tmp_dir}" ] && rm -rf "${tmp_dir}"
+        mkdir "${tmp_dir}"
+        cp packages/* "fyne-cross/bin/linux-${arch}/infraspace-savegame-editor" "${tmp_dir}/"
+        tar -C "${tmp_dir}" -czf "fyne-cross/dist/infraspace-savegame-editor_linux-${arch}.tar.gz" .
+        rm -rf "${tmp_dir}"
     elif [ "${os}" == "windows" ]; then
         mv "fyne-cross/dist/windows-${arch}/InfraSpace Savegame Editor.exe.zip" "fyne-cross/dist/infraspace-savegame-editor_windows-${arch}.zip"
         rm -rf "fyne-cross/dist/windows-${arch}"
