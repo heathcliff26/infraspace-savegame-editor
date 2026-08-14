08f3835478b97df5e9712d545feb9497983ddfa8
Heathcliff <heathcliff@heathcliff.eu>
2026-08-14 15:59:10 +0200

    CI: Switch to containerized script
    Instead of invoking podman in multiple different scripts, use a single one.
    Additionally pin the image to a specific tag.
    
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- hack/coverprofile.sh
+++ hack/coverprofile.sh
@@ -6,13 +6,13 @@ base_dir="$(dirname "${BASH_SOURCE[0]}" | xargs realpath)/.."
 
 pushd "${base_dir}" >/dev/null
 
-OUT_DIR="coverprofiles"
+OUT_DIR="${base_dir}/coverprofiles"
 
 if [ ! -d "${OUT_DIR}" ]; then
     mkdir "${OUT_DIR}"
 fi
 
-"${base_dir}/hack/unit-tests.sh"
+make test
 go tool cover -html "coverprofile.out" -o "${OUT_DIR}/index.html"
 
 popd >/dev/null
